a8128cfaf35c8b0c0d803770f452a704d338cdd6
Stefan Nikolovski <ni.stefan.95@gmail.com>
2022-09-18 21:12:05 +0200

    heading, vision, team, services sections done

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,40 +1,55 @@
 import Testimonials from "../../components/Testimonials/Testimonials";
+import "./About.scss";
 
 function About() {
   return (
     <main>
-      <section className="heading">
+      <section className="header">
         <div className="container">About Us</div>
       </section>
 
       <section className="vision">
         <div className="container">
           <h2>Your Vision Unrestricted</h2>
-          <p>
-            Houzez is a premium WordPress theme for real estate agents and
-            agencies where modern aesthetics are combined with a tasteful
-            simplicity and where the ease of use is achieved without compromise
-            in your ability to customize the design.
-          </p>
-          <p>
-            Whether you are a real estate agent looking to build a website for
-            your company or a web developer seeking a perfect WordPress theme
-            for your next project, you are certain to appreciate the numerous
-            features and benefits that our theme provides.
-          </p>
-          <p>
-            Houzez is also a WordPress-based property management system which
-            allows you to own and maintain a real estate marketplace, coordinate
-            your agents, accept submissions and offer membership packages.
-          </p>
-          <p>
-            Unlike many other real estate themes which confine you to a handful
-            of predefined layouts, Houzez offers a limitless array of
-            possibilities to structure and style your content. All of the
-            customization options are logically organized in your WordPress
-            admin panel and thorough customization in the provided
-            documentation.
-          </p>
+          <div className="vision-body">
+            <div className="column">
+              <p>
+                <span>
+                  Houzez is a premium WordPress theme for real estate agents and
+                  agencies
+                </span>{" "}
+                where modern aesthetics are combined with a tasteful simplicity
+                and where the ease of use is achieved without compromise in your
+                ability to customize the design.
+              </p>
+              <p>
+                Whether you are a real estate agent looking to build a website
+                for your company or a web developer seeking a perfect WordPress
+                theme for your next project, you are certain to appreciate the
+                numerous features and benefits that our theme provides.
+              </p>
+            </div>
+            <div className="column">
+              <p>
+                Houzez is also a WordPress-based property management system
+                which allows you to own and maintain a real estate marketplace,
+                coordinate your agents, accept submissions and offer membership
+                packages.
+              </p>
+              <p>
+                Unlike many other real estate themes which confine you to a
+                handful of predefined layouts,
+                <span>
+                  {" "}
+                  Houzez offers a limitless array of possibilities to structure
+                  and style your content.
+                </span>{" "}
+                All of the customization options are logically organized in your
+                WordPress admin panel and thorough customization in the provided
+                documentation.
+              </p>
+            </div>
+          </div>
         </div>
       </section>
 
@@ -49,36 +64,108 @@ function About() {
           </div>
           <div className="our-team-body">
             <div className="employee-card">
-              <div className="card-overlay"></div>
               <a href="">
                 <img src="/images/team/member-1.jpg" alt="" />
               </a>
-              <p>Kathleen Grant</p>
-              <p>Funder</p>
+              <div className="card-details">
+                <div className="card-overlay"></div>
+                <p>Kathleen Grant</p>
+                <p>Funder</p>
+              </div>
+              <div className="hover-details">
+                <p>Kathleen Grant</p>
+                <p>Funder</p>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                  Natus libero voluptate minima voluptates perferendis
+                  blanditiis nostrum totam quaerat eos nobis?
+                </p>
+                <div className="socials">
+                  <i className="fa-brands fa-facebook-f"></i>
+                  <i className="fa-brands fa-twitter"></i>
+                  <i className="fa-brands fa-google-plus-g"></i>
+                  <i className="fa-brands fa-linkedin-in"></i>
+                  <i className="fa-brands fa-instagram"></i>
+                </div>
+              </div>
             </div>
             <div className="employee-card">
-              <div className="card-overlay"></div>
               <a href="">
                 <img src="/images/team/member-2.jpg" alt="" />
               </a>
-              <p>Keith Bailey</p>
-              <p>CEO</p>
+              <div className="card-details">
+                <div className="card-overlay"></div>
+                <p>Keith Bailey</p>
+                <p>CEO</p>
+              </div>
+              <div className="hover-details">
+                <p>Keith Bailey</p>
+                <p>CEO</p>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                  Natus libero voluptate minima voluptates perferendis
+                  blanditiis nostrum totam quaerat eos nobis?
+                </p>
+                <div className="socials">
+                  <i className="fa-brands fa-facebook-f"></i>
+                  <i className="fa-brands fa-twitter"></i>
+                  <i className="fa-brands fa-google-plus-g"></i>
+                  <i className="fa-brands fa-linkedin-in"></i>
+                  <i className="fa-brands fa-instagram"></i>
+                </div>
+              </div>
             </div>
             <div className="employee-card">
-              <div className="card-overlay"></div>
               <a href="">
                 <img src="/images/team/member-3.jpg" alt="" />
               </a>
-              <p>Danielle Murray</p>
-              <p>Manager</p>
+              <div className="card-details">
+                <div className="card-overlay"></div>
+                <p>Danielle Murray</p>
+                <p>Manager</p>
+              </div>
+              <div className="hover-details">
+                <p>Danielle Murray</p>
+                <p>Manager</p>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                  Natus libero voluptate minima voluptates perferendis
+                  blanditiis nostrum totam quaerat eos nobis?
+                </p>
+                <div className="socials">
+                  <i className="fa-brands fa-facebook-f"></i>
+                  <i className="fa-brands fa-twitter"></i>
+                  <i className="fa-brands fa-google-plus-g"></i>
+                  <i className="fa-brands fa-linkedin-in"></i>
+                  <i className="fa-brands fa-instagram"></i>
+                </div>
+              </div>
             </div>
             <div className="employee-card">
-              <div className="card-overlay"></div>
               <a href="">
                 <img src="/images/team/member-4.jpg" alt="" />
               </a>
-              <p>Thomas Stevenes</p>
-              <p>Manager</p>
+              <div className="card-details">
+                <div className="card-overlay"></div>
+                <p>Thomas Stevenes</p>
+                <p>Manager</p>
+              </div>
+              <div className="hover-details">
+                <p>Thomas Stevenes</p>
+                <p>Manager</p>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                  Natus libero voluptate minima voluptates perferendis
+                  blanditiis nostrum totam quaerat eos nobis?
+                </p>
+                <div className="socials">
+                  <i className="fa-brands fa-facebook-f"></i>
+                  <i className="fa-brands fa-twitter"></i>
+                  <i className="fa-brands fa-google-plus-g"></i>
+                  <i className="fa-brands fa-linkedin-in"></i>
+                  <i className="fa-brands fa-instagram"></i>
+                </div>
+              </div>
             </div>
           </div>
         </div>
@@ -94,7 +181,7 @@ function About() {
             </p>
           </div>
           <div className="services-body">
-            <div className="box-item">
+            <div className="box-item small-box">
               <div className="box-overlay"></div>
               <div className="box-title">
                 <p>Services</p>
@@ -105,7 +192,7 @@ function About() {
                 <i className="fa-solid fa-caret-right"></i>
               </div>
             </div>
-            <div className="box-item">
+            <div className="box-item small-box">
               <div className="box-overlay"></div>
               <div className="box-title">
                 <p>Services</p>
@@ -116,7 +203,7 @@ function About() {
                 <i className="fa-solid fa-caret-right"></i>
               </div>
             </div>
-            <div className="box-item">
+            <div className="box-item small-box">
               <div className="box-overlay"></div>
               <div className="box-title">
                 <p>Services</p>
@@ -127,7 +214,7 @@ function About() {
                 <i className="fa-solid fa-caret-right"></i>
               </div>
             </div>
-            <div className="box-item">
+            <div className="box-item small-box">
               <div className="box-overlay"></div>
               <div className="box-title">
                 <p>Services</p>
@@ -138,7 +225,7 @@ function About() {
                 <i className="fa-solid fa-caret-right"></i>
               </div>
             </div>
-            <div className="box-item">
+            <div className="box-item big-box">
               <div className="box-overlay"></div>
               <div className="box-title">
                 <p>Services</p>
@@ -149,7 +236,7 @@ function About() {
                 <i className="fa-solid fa-caret-right"></i>
               </div>
             </div>
-            <div className="box-item">
+            <div className="box-item big-box">
               <div className="box-overlay"></div>
               <div className="box-title">
                 <p>Services</p>
