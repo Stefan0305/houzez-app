6951a5159e2913507d89160b2acc2f2b34a20e09
Stefan Nikolovski <ni.stefan.95@gmail.com>
2022-10-04 22:22:50 +0200

    added back to top button

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import logo from "./logo.svg";
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import "./App.scss";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
@@ -7,12 +7,30 @@ import About from "./pages/About/About";
 import Contact from "./pages/Contact/Contact";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
-function App() {
+import ScrollToTop from "react-scroll-to-top";
 
+function App() {
   return (
     <div>
+      <ScrollToTop
+        smooth
+        top={300}
+        style={{
+          backgroundColor: "rgb(0, 176, 240)",
+          color: "white",
+          borderRadius: "3px",
+          fontSize: "24px",
+          width: "42px",
+          height: "42px",
+          bottom: "30px",
+          right: "30px",
+          boxShadow: "none",
+          zIndex: 1000,
+        }}
+        component={<i className="fa-solid fa-angle-up"></i>}
+      />
       <BrowserRouter>
-      <Header/>
+        <Header />
         <Routes>
           <Route path="/" element={<Home />}></Route>
           <Route path="/contact" element={<Contact />}></Route>
